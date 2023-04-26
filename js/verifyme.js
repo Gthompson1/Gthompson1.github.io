ed7cac03eea70b7aec5f7368aca62f83d3da8aae
Gthompson1 <120082443+Gthompson1@users.noreply.github.com>
2023-04-26 10:49:31 -0600

    april 25

---
+++ js/verifyme.js
@@ -0,0 +1,12 @@
+var age;
+
+age = prompt("")
+if (age == "test") {
+    output = "Free Friday Coffee Night for Seniors!"
+    document.getElementById('verify').style.color = 'maroon';
+    document.getElementById('verify').style.fontVariantCaps = 'small-caps';
+} else {
+    NamedNodeMap
+}
+
+document.getElementById("verify").innerHTML = output;
